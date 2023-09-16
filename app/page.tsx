6220919cab4f1e5b947696aa747951865a907c0f
Hunter <hunterl3535@yahoo.com>
2023-09-16 17:30:33 -0500

    CI=false

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
 import Header from "./header";
-import FrontPage from "./FrontPage";
-import Projects from "./Projects";
-import ContactMe from "./ContactMe";
 import Footer from "./Footer";
-import PageMarkers from "./PageMarkers";
-import AboutMe from "./AboutMe";
 import HomePage from "./HomePage";
 
 export default function Home(){
