550e247c20803cfc2e6bde695d11c06ffb6a06bb
Hunter <hunterl3535@yahoo.com>
2023-09-24 12:54:32 -0500

    added resume button

--- app/FrontPage.tsx
+++ app/FrontPage.tsx
@@ -4,12 +4,12 @@ export default function FrontPage(){
     return(
         <section className="flex">
             <section className="w-1/2 h-screen flex flex-col justify-center">
-                <section className="w-1/2 m-auto">
+                <section className=" m-auto flex flex-col">
                     <section className="flex">
                         <h1 className="text-8xl text-red-600">Hi</h1>
                         <h1 className="text-8xl ">, Im</h1>
                     </section>
-                    <h1 className="text-8xl ">Hunter</h1>
+                    <h1 className="text-8xl mb-5">Hunter</h1>
                 </section>
             </section>
         <section className="w-1/2 h-screen flex flex-col justify-center overflow-hidden">
--- app/header.tsx
+++ app/header.tsx
@@ -11,19 +11,27 @@ export default function Header(){
   }
 
   return(
-    <header className="flex scrollSnap "> 
+    <header className="flex scrollSnap  header"> 
       <button onClick={handleHomeButton}>
-        <h1 className="mr-40 text-3xl mt-5">Hunter Lindsay</h1>
+        <h1 className="mr-40  mt-5 text-2xl">Hunter Lindsay</h1>
       </button>
-      <h1 className="mr-10 text-3xl mt-5">214-608-3624</h1>
-      <h1 className="mr-10 text-3xl mt-5">/</h1>
-      <h1 className="text-3xl mt-5">Hunterl3535@gmail.com</h1>
-      <button
-        type="button" 
-        onClick={() => router.push('mailto:hunterl3535@gmail.com')}
-        className="flex ml-auto text-red-600">
-        <h1 className="text-3xl mt-5 mr-5">Contact Me</h1>
-      </button>
+      <h1 className="mr-10  mt-5 text-2xl">214-608-3624</h1>
+      <h1 className="mr-10  mt-5 text-2xl">/</h1>
+      <h1 className=" mt-5 text-2xl">Hunterl3535@gmail.com</h1>
+      <section className="flex ml-auto">
+        <a
+          type="button" 
+          href="HunterLindsayTechnicalResume.pdf"
+          className="flex ml-auto mr-10 text-red-600 text-2xl">
+          <h1 className=" mt-5 ">Resume</h1>
+        </a>
+        <button
+          type="button" 
+          onClick={() => router.push('mailto:hunterl3535@gmail.com')}
+          className="flex ml-auto text-red-600 text-2xl">
+          <h1 className=" mt-5 mr-5">Contact Me</h1>
+        </button>
+      </section>
     </header>
   )
 }
